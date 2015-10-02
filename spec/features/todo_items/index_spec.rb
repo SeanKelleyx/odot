329555499624c60f4f4fce6983a1ba69711225d2
Sean Kelley <s.kelley27@gmail.com>
2015-10-01 21:52:12 -0500

    Adding tests for list items

--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -3,13 +3,34 @@ require 'rails_helper'
 describe "Viewing todo items" do
 	let!(:todo_list){TodoList.create(title: "Groceries", description: "Grocery list.")}
 
-	it "displays no items when an list is empty" do 
+	def visit_todo_list(list)
 		visit "/todo_lists"
 
-		within "#todo_list_#{todo_list.id}" do
+		within "#todo_list_#{list.id}" do
 			click_link "List Items"
 		end
+	end
 
-		expect(page).to have_content("TodoItems#index")
+	it "displays the list title on list items page" do 
+		visit_todo_list(todo_list)
+		within("h1") do
+			expect(page).to have_content(todo_list.title)
+		end
+	end
+
+	it "displays no items when an list is empty" do 
+		visit_todo_list(todo_list)
+		expect(page.all("ul.todo_items li").size).to eq(0)
+	end
+
+	it "displays content when there are items in a list" do
+		todo_list.todo_item.create(content: "Eggs")
+		todo_list.todo_item.create(content: "Bacon")
+		visit_todo_list(todo_list)
+		expect(page.all("ul.todo_items li").size).to eq(2)
+		within "ul.todo_items" do
+			expect(page).to have_content("Eggs")
+			expect(page).to have_content("Bacon")
+		end
 	end
 end 
