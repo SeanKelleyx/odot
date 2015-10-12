a2664db273414842532af0b3937b60136f172b8b
Sean Kelley <s.kelley27@gmail.com>
2015-10-11 22:46:47 -0500

    Cleaning up UI

--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -12,14 +12,14 @@ describe "Viewing todo items" do
 
 	it "displays no items when an list is empty" do 
 		visit_todo_list(todo_list)
-		expect(page.all("table.todo_items tr").size).to eq(0)
+		expect(page.all("table.todo_items tbody tr").size).to eq(0)
 	end
 
 	it "displays content when there are items in a list" do
 		todo_list.todo_item.create(content: "Eggs")
 		todo_list.todo_item.create(content: "Bacon")
 		visit_todo_list(todo_list)
-		expect(page.all("table.todo_items tr").size).to eq(2)
+		expect(page.all("table.todo_items tbody tr").size).to eq(2)
 		within "table.todo_items" do
 			expect(page).to have_content("Eggs")
 			expect(page).to have_content("Bacon")
--- spec/models/todo_list_spec.rb
+++ spec/models/todo_list_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe TodoList, type: :model do
   it { should have_many(:todo_item)}
+
+  describe "#has_complete_items?" do
+    let!(:todo_list){TodoList.create(title: "Groceries", description: "Grocery list.")}
+    
+    it "is false with no completed itmes" do
+      todo_list.todo_item.create(content:"Eggs")
+      expect(todo_list.has_completed_items?).to  be false
+    end
+
+    it "is true with completed todo items" do
+      todo_list.todo_item.create(content:"Eggs", completed_at: 1.minutes.ago)
+      expect(todo_list.has_completed_items?).to  be true
+    end
+  end
+
+
 end
