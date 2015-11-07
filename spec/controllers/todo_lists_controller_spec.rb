4ad0bc0c9506eb010961c25b6f428c17ec832a3e
Sean Kelley <s.kelley27@gmail.com>
2015-11-06 22:04:10 -0600

    fix todo lists controller spec

--- spec/controllers/todo_lists_controller_spec.rb
+++ spec/controllers/todo_lists_controller_spec.rb
@@ -52,7 +52,7 @@ RSpec.describe TodoListsController, type: :controller do
     #  end
     #end
     it "assigns all todo_lists as @todo_lists" do
-      todo_list = TodoList.create! valid_attributes
+      todo_list = user.todo_lists.create! valid_attributes
       get :index, {}, valid_session
       expect(assigns(:todo_lists)).to eq([todo_list])
     end
@@ -64,7 +64,7 @@ RSpec.describe TodoListsController, type: :controller do
         allow(controller).to receive(:require_user).and_return(true)
       end
       it "assigns the requested todo_list as @todo_list" do
-        todo_list = TodoList.create! valid_attributes
+        todo_list = user.todo_lists.create! valid_attributes
         get :show, {:id => todo_list.to_param}, valid_session
         expect(assigns(:todo_list)).to eq(todo_list)
       end
@@ -80,7 +80,7 @@ RSpec.describe TodoListsController, type: :controller do
 
   describe "GET #edit" do
     it "assigns the requested todo_list as @todo_list" do
-      todo_list = TodoList.create! valid_attributes
+      todo_list = user.todo_lists.create! valid_attributes
       get :edit, {:id => todo_list.to_param}, valid_session
       expect(assigns(:todo_list)).to eq(todo_list)
     end
@@ -132,19 +132,19 @@ RSpec.describe TodoListsController, type: :controller do
       }}
 
       it "updates the requested todo_list" do
-        todo_list = TodoList.create! valid_attributes
+        todo_list = user.todo_lists.create! valid_attributes
         put :update, {:id => todo_list.to_param, :todo_list => new_attributes}, valid_session
         todo_list.reload
       end
 
       it "assigns the requested todo_list as @todo_list" do
-        todo_list = TodoList.create! valid_attributes
+        todo_list = user.todo_lists.create! valid_attributes
         put :update, {:id => todo_list.to_param, :todo_list => valid_attributes}, valid_session
         expect(assigns(:todo_list)).to eq(todo_list)
       end
 
       it "redirects to the todo_list" do
-        todo_list = TodoList.create! valid_attributes
+        todo_list = user.todo_lists.create! valid_attributes
         put :update, {:id => todo_list.to_param, :todo_list => valid_attributes}, valid_session
         expect(response).to redirect_to(todo_list)
       end
@@ -152,13 +152,13 @@ RSpec.describe TodoListsController, type: :controller do
 
     context "with invalid params" do
       it "assigns the todo_list as @todo_list" do
-        todo_list = TodoList.create! valid_attributes
+        todo_list = user.todo_lists.create! valid_attributes
         put :update, {:id => todo_list.to_param, :todo_list => invalid_attributes}, valid_session
         expect(assigns(:todo_list)).to eq(todo_list)
       end
 
       it "re-renders the 'edit' template" do
-        todo_list = TodoList.create! valid_attributes
+        todo_list = user.todo_lists.create! valid_attributes
         put :update, {:id => todo_list.to_param, :todo_list => invalid_attributes}, valid_session
         expect(response).to render_template("edit")
       end
@@ -167,14 +167,14 @@ RSpec.describe TodoListsController, type: :controller do
 
   describe "DELETE #destroy" do
     it "destroys the requested todo_list" do
-      todo_list = TodoList.create! valid_attributes
+      todo_list = user.todo_lists.create! valid_attributes
       expect {
         delete :destroy, {:id => todo_list.to_param}, valid_session
       }.to change(TodoList, :count).by(-1)
     end
 
     it "redirects to the todo_lists list" do
-      todo_list = TodoList.create! valid_attributes
+      todo_list = user.todo_lists.create! valid_attributes
       delete :destroy, {:id => todo_list.to_param}, valid_session
       expect(response).to redirect_to(todo_lists_url)
     end
