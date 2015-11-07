88f04ebc93e82a90adb0c76071616e1f86ff471a
Sean Kelley <s.kelley27@gmail.com>
2015-11-07 16:16:05 -0600

    add views, routes and methods for password resets, spec still failing

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get "/logout" => "user_sessions#destroy", as: :logout
   
   resources :user_sessions, only: [:new,:create]
+  resources :password_resets, only: [:new, :create]
 
   resources :users
   resources :todo_lists do 
